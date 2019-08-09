744d3f78dc508521e38044805a70baf2d5280854
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-08-09 15:25:13 -0400

    Done 100% coverage

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,11 +1,80 @@
 import pytest
 
-from mapsdg.components import ImageShape
+from mapsdg.components import GeocodedLocation, ImageShape, StaticMapType, \
+    ImageFormat, LatLon, ViewType, Heading, FieldOfView, Pitch, Radius
 
 
 def fail_if(boolean):
     if boolean:
         pytest.fail()
+
+
+def test_heading():
+    heading = Heading()
+    fail_if(heading.value != 0)
+
+    try:
+        Heading(-1)
+        pytest.fail()
+    except ValueError:
+        pass
+    try:
+        Heading(361)
+        pytest.fail()
+    except ValueError:
+        pass
+
+    fail_if(Heading(200).value != 200)
+
+
+def test_field_of_fiew():
+    fov = FieldOfView()
+    fail_if(fov.value != 90)
+
+    try:
+        FieldOfView(-1)
+        pytest.fail()
+    except ValueError:
+        pass
+    try:
+        FieldOfView(361)
+        pytest.fail()
+    except ValueError:
+        pass
+
+    fail_if(FieldOfView(10).value != 10)
+
+
+def test_pitch():
+    pitch = Pitch()
+    fail_if(pitch.value != 0)
+
+    try:
+        Pitch(-100)
+        pytest.fail()
+    except ValueError:
+        pass
+    try:
+        Pitch(361)
+        pytest.fail()
+    except ValueError:
+        pass
+
+    fail_if(Pitch(10).value != 10)
+    fail_if(Pitch(-10).value != -10)
+
+
+def test_radius():
+    radius = Radius()
+    fail_if(radius.value != 50)
+
+    try:
+        Radius(-100)
+        pytest.fail()
+    except ValueError:
+        pass
+
+    fail_if(Radius(10).value != 10)
 
 
 def test_image_shape():
@@ -30,3 +99,9 @@ def test_image_shape():
             pass
         except Exception:
             pytest.fail()
+
+
+def test_lat_lon():
+    x = LatLon(lon=100, lat=200)
+    fail_if(x.lon != 100)
+    fail_if(x.lat != 200)
