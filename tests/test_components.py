8580de30fea76b7536ae53a1b3892773d826153d
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-08-09 14:40:55 -0400

    Work on tests

--- tests/test_components.py
+++ tests/test_components.py
@@ -3,27 +3,29 @@ import pytest
 from mapsdg.components import ImageShape
 
 
+def fail_if(boolean):
+    if boolean:
+        pytest.fail()
+
+
 def test_image_shape():
     try:
-        img_shape = ImageShape(w=100, h=100)
-        if img_shape.w != 100 or img_shape.h != 100:
-            pytest.fail()
+        img_shape = ImageShape(width=100, height=100)
+        fail_if(img_shape.width != 100 or img_shape.height != 100)
     except Exception:
         pytest.fail()
     for i in [-1, 0]:
         try:
-            img_shape = ImageShape(w=i, h=100)
-            if img_shape.w != i or img_shape.h != 100:
-                pytest.fail()
+            img_shape = ImageShape(width=i, height=100)
+            fail_if(img_shape.width != i or img_shape.height != 100)
         except ValueError:
             pass
         except Exception:
             pytest.fail()
     for i in [-1, 0]:
         try:
-            img_shape = ImageShape(w=100, h=i)
-            if img_shape.w != 100 or img_shape.h != i:
-                pytest.fail()
+            img_shape = ImageShape(width=100, height=i)
+            fail_if(img_shape.width != 100 or img_shape.height != i)
         except ValueError:
             pass
         except Exception:
--- tests/test_google_maps_api.py
+++ tests/test_google_maps_api.py
@@ -1,9 +1,39 @@
 import importlib.resources
+import pytest
+
 
 from mapsdg.google_maps_api import GoogleMapsAPI
+from mapsdg.components import GeocodedLocation, ImageShape, StaticMapType, \
+    ImageFormat, LatLon, ViewType, Heading, FieldOfView, Pitch, Radius
 
 API_KEY = importlib.resources.read_text('mapsdg', '.api_key')
+GAPI = GoogleMapsAPI(API_KEY)
+
+
+def fail_if(boolean):
+    if boolean:
+        pytest.fail()
+
+
+def test_geocode_address():
+    global GAPI
 
 
 def test_get_static_map():
-    return
+    global GAPI
+    geocoded = GAPI.geocode_address('233 Soper Place, Ottawa, Ontario, Canada')
+    url = GAPI.get_image_url(
+        addr='233 Soper Place, Ottawa, Ontario, Canada',
+        view_type=ViewType.staticmap,)
+
+    expected_url = (
+        f"https://maps.googleapis.com/maps/api/staticmap?" +
+        f"center={geocoded.lat},{geocoded.lon}&" +
+        f"zoom=20&" +
+        f"size=640x400&" +
+        f"maptype=satellite&" +
+        f"format=png&" +
+        "style=feature%3Aall%7Celement%3Alabels%7Cvisibility%3Aoff&" +
+        f"key={API_KEY}".strip())
+
+    fail_if(url != expected_url)
